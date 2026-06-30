60b4aff19a3eae9126f95ec790bc551c62641dc7
Oriol599 <arge_prog@proton.me>
2026-06-30 13:04:08 +0200

    pujada del block 7

--- exercises/07_object-literal.test.js
+++ exercises/07_object-literal.test.js
@@ -9,7 +9,18 @@ test('07_object-literal-1: can use shorthand for property names', () => {
     //     return `${this.name} attacked ${target.name}`;
     //   }
     // }
+
+    return{
+      type: 'Monster',
+      name,
+      power,
+      attack(target){
+        return `${this.name} attacked ${target.name}`
+      }
+    }
   }
+
+
 
   const godzilla = createMonster('Godzilla', 1000)
   const mechaGodzilla = createMonster('MechaGodzilla', 5000)
@@ -25,6 +36,7 @@ test('07_object-literal-2: can use expressions as property names', () => {
       type,
       // afegeix una expressió com a nom de propietat on el nom de la propietat sigui el resultat de type.toUpperCase() + type.length
       // sembla forçat? Ho és... 😅
+      [type.toUpperCase() + type.length]: description
     }
   }
 
