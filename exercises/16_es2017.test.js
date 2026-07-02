84dcc8b1521c6bb4314fad518927964bed42f260
Oriol599 <arge_prog@proton.me>
2026-07-02 10:27:57 +0200

    Pujada del block 16

--- exercises/16_es2017.test.js
+++ exercises/16_es2017.test.js
@@ -1,7 +1,8 @@
 test('16_es2016-1: the exponentiation operation can be used to raise a number to a power of another number', () => {
   // refactoritza amb l'operador exponencial (**)
 
-  const result = Math.pow(3, 2)
+  // const result = Math.pow(3, 2)
+  const result = 3**2
   expect(result).toBe(9)
 })
 
@@ -14,20 +15,25 @@ test('16_es2016-2: array.includes can be used to determine whether an item exist
     {name: 'Ingvar Stepanyan'},
   ]
   // refactoritza  amb `includes`
-  const result = greatFriends.indexOf(bestFriend) !== -1
+  // const result = greatFriends.indexOf(bestFriend) !== -1
+  const result = greatFriends.includes(bestFriend);
+
   expect(result).toBe(true)
 })
 
 test('16_es2017-1: String.prototype.padStart saves us from left-pad-gate', () => {
   const originalString = 'Worlds Finest'
   // crida a padStart en aquesta cadena per fer que el test passi
-  const result = originalString
+  // const result = originalString
+  const result = originalString.padStart(4,"Worlds Finest")
+
   expect(result).toBe('    Worlds Finest')
 })
 
 test('16_es2017-2: String.prototype.padEnd (and padStart) can be given a string to pad with', () => {
-  const originalString = 'Stronger Together'
+  // const originalString = 'Stronger Together'
   // crida a padEnd en aquesta cadena per fer que el test passi
+  const result = originalString.padEnd(10, "-123-123-1")
   expect(result).toBe('Stronger Together-123-123-1')
 })
 
@@ -45,6 +51,9 @@ test('16_es2017-3: Object.values gets just the values of an object', () => {
       'Alex Danvers',
     ],
   }
+
+  const result = Object.values(show);
+
   // obté els valors de l'objecte show com un array
   expect(result).toEqual([
     'Supergirl',
@@ -75,6 +84,8 @@ test('16_es2017-4: Object.entries gives an array of arrays as [key, value]', () 
       'Joe West',
     ],
   }
+
+  const result = Object.keys(show); 
   // obté un array [key, value] de l'objecte show
   expect(result).toEqual([
     ['title', 'The Flash'],
@@ -101,7 +112,7 @@ test('16_es2017-5: Trailing commas in function parameter lists and calls help us
     function foo(
       a,
       b,
-      c
+      c,
     ) {
       log(a, b, c)
     }
@@ -109,23 +120,23 @@ test('16_es2017-5: Trailing commas in function parameter lists and calls help us
     foo(
       1,
       2,
-      3
+      3,
     )
 
     function bar(
       a,
       b,
-      ...rest,
+      ...rest
     ) {
       log(a, b, ...rest)
     }
     bar(
       1, 2, 3,
-      4, 5, 6
+      4, 5, 6,
     )
 
     function log() {
-      // no facis res :)
+      
     }
   `).toBeValidSyntax()
 })
