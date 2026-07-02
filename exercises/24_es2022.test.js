014fc9c8b6eb8f93211b2e540849eebc50bad63f
Oriol599 <arge_prog@proton.me>
2026-07-02 18:19:10 +0200

    Pujada del block 24

--- exercises/24_es2022.test.js
+++ exercises/24_es2022.test.js
@@ -1,22 +1,27 @@
-test('24_es2022-1: Class fields and private methods', () => {
+test("24_es2022-1: Class fields and private methods", () => {
   // Crea una classe amb un camp privat i un mètode privat. Afegeix un getter per accedir al camp privat.
   class Person {
-    // Resol aquí...
+    #secret = "secret";
+    static publicField = "public";
+
+    getSecret() {
+      return this.#secret;
+    }
   }
 
-  const person = new Person()
-  expect(person.getSecret()).toBe('secret')
-  expect(Person.publicField).toBe('public')
-})
+  const person = new Person();
+  expect(person.getSecret()).toBe("secret");
+  expect(Person.publicField).toBe("public");
+});
 
-test('24_es2022-2: at() method for indexing arrays and strings', () => {
+test("24_es2022-2: at() method for indexing arrays and strings", () => {
   // Utilitza el mètode at() per accedir als elements d'un array i una cadena.
-  const array = [1, 2, 3, 4]
-  const string = 'hello'
+  const array = [1, 2, 3, 4];
+  const string = "hello";
 
-  const lastArrayElement = null // Resol aquí...
-  const secondLastStringChar = null // Resol aquí...
+  const lastArrayElement = array.at(3);
+  const secondLastStringChar = string.at(2);
 
-  expect(lastArrayElement).toBe(4)
-  expect(secondLastStringChar).toBe('l')
-})
+  expect(lastArrayElement).toBe(4);
+  expect(secondLastStringChar).toBe("l");
+});
